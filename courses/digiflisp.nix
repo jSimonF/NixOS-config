4baaa5cabb2aaf33196a3956b12cdc5b4b884ca6
JSimonF <jsimonfranklin@hotmail.com>
2024-11-02 23:10:04 +0100

    Added digiflisp

--- courses/digiflisp.nix
+++ courses/digiflisp.nix
@@ -12,15 +12,30 @@ let
     url = "http://www.gbgmv.se/dl/linux/digiflisp_2.06.tar.gz";
     sha256 = "1vxk5yj79brjlfd5c4hjqpnspxrr0v4sja1r94mydvcmrskw8z4x";
   };
+
 in
-  runCommand "digiflisp" {
+{
+  # Build digiflisp and qaflisp
+  digiflisp = runCommand "digiflisp" {
     nativeBuildInputs = [ autoPatchelfHook ];
     buildInputs = [ xorg.libX11 xorg.libXxf86vm xorg.libSM libxkbcommon gtk3 ];
   } ''
-    set -e  # Exit immediately if a command exits with a non-zero status
+    set -e
     mkdir -p $out/bin
     tar -xvf ${src} -C $out/bin || { echo "Extraction failed"; exit 1; }
-    chmod +x $out/bin/*  # Make sure all binaries are executable
+    chmod +x $out/bin/*  
     autoPatchelf $out/bin/*
-  ''
+  '';
+
+  qaflisp = runCommand "qaflisp" {
+    nativeBuildInputs = [ autoPatchelfHook ];
+    buildInputs = [ xorg.libX11 xorg.libXxf86vm xorg.libSM libxkbcommon gtk3 ];
+  } ''
+    set -e
+    mkdir -p $out/bin
+    tar -xvf ${src} -C $out/bin || { echo "Extraction failed"; exit 1; }
+    chmod +x $out/bin/*  
+    autoPatchelf $out/bin/*
+  '';
+}
 
