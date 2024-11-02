6a39b3fb5140482d4bb4aa270a571100ef7a3282
JSimonF <jsimonfranklin@hotmail.com>
2024-11-02 23:00:19 +0100

    Added digiflisp

--- courses/digiflisp.nix
+++ courses/digiflisp.nix
@@ -1,18 +1,26 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.stdenv.mkDerivation {
-  pname = "digiflisp";
-  version = "VERSION";
+let
+  fetchurl = pkgs.fetchurl;
+  runCommand = pkgs.runCommand;
+  autoPatchelfHook = pkgs.autoPatchelfHook;
+  xorg = pkgs.xorg;
+  libxkbcommon = pkgs.libxkbcommon;
+  gtk3 = pkgs.gtk3;
 
-  src = pkgs.fetchurl {
+  src = fetchurl {
     url = "http://www.gbgmv.se/dl/linux/digiflisp_2.06.tar.gz";
     sha256 = "1vxk5yj79brjlfd5c4hjqpnspxrr0v4sja1r94mydvcmrskw8z4x";
   };
-
-  nativeBuildInputs = [ pkgs.autoPatchelfHook ];
-
-  installPhase = ''
+in
+  runCommand "digiflisp" {
+    nativeBuildInputs = [ autoPatchelfHook ];
+    buildInputs = [ xorg.libX11 xorg.libXxf86vm xorg.libSM libxkbcommon gtk3 ];
+  } ''
+    set -e  # Exit immediately if a command exits with a non-zero status
     mkdir -p $out/bin
-    tar -xzvf $src --strip-components=1 -C $out/bin
-  '';
-}
+    tar -xvf ${src} -C $out/bin || { echo "Extraction failed"; exit 1; }
+    chmod +x $out/bin/*  # Make sure all binaries are executable
+    autoPatchelf $out/bin/*
+  ''
+
