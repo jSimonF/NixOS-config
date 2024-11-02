29feb6fdc2982f311e29df50cd3f23c4e54739ec
JSimonF <jsimonfranklin@hotmail.com>
2024-11-02 23:25:37 +0100

    Added digiflisp

--- courses/grudat.nix
+++ courses/grudat.nix
@@ -8,28 +8,5 @@ in
     digiflisp.digiflisp
     digiflisp.qaflisp
   ];
-
-  # Adding .desktop entries for Rofi
-  environment.xdg.applications.digiflisp = ''
-    [Desktop Entry]
-    Version=1.0
-    Name=Digiflisp
-    Exec=${digiflisp.digiflisp}/bin/digiflisp
-    Icon=/path/to/icon.png  # Adjust as needed
-    Type=Application
-    Terminal=false
-    Categories=Development;IDE;
-  '';
-
-  environment.etc.xdg.applications.qaflisp = ''
-    [Desktop Entry]
-    Version=1.0
-    Name=Qaflisp
-    Exec=${digiflisp.qaflisp}/bin/qaflisp
-    Icon=/path/to/icon.png  # Adjust as needed
-    Type=Application
-    Terminal=false
-    Categories=Development;IDE;
-  '';
 }
 
