5edc322649911b687de48e41c83bb3b73bbc7398
JSimonF <jsimonfranklin@hotmail.com>
2024-12-19 14:18:06 +0100

    Updated flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
     description = "Simon's flake";
 
     inputs = {
-      nixpkgs.url = "nixpkgs/nixos-24.11";
+      nixpkgs.url = "nixpkgs/nixos-24.05";
       nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-      home-manager.url = "github:nix-community/home-manager/release-24.11";
+      home-manager.url = "github:nix-community/home-manager/release-24.05";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
-      stylix.url = "github:danth/stylix/release-24.11";
+      stylix.url = "github:danth/stylix/release-24.05";
     };
 
     outputs = { self, nixpkgs, nixos-hardware, home-manager, stylix, ... }@inputs:
